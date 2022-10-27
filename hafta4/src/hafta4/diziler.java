80f2b41ba040102d72235d20c03e05cff529b625
aidata <aidata@10.121.3.199>
2022-10-27 10:50:02 +0300

    adasdsad

--- hafta4/src/hafta4/diziler.java
+++ hafta4/src/hafta4/diziler.java
@@ -1,24 +1,31 @@
 package hafta4;
 
+import java.lang.reflect.Array;
+
 public class diziler {
 
      
 	
   public static void main(String[]args) {
 	  
-	  int[] array=new int[10];
+	final int ARRAY_LENGTH=10;
+	
+	
+	int[] array = new int[ARRAY_LENGTH];
+	
+	
+	for (int counter=0;counter<array.length;counter++) {
+		
+		
+		array[counter]=2+2*counter;
+		}
 	  
-	  
-	  System.out.printf("%s%8%n","Index","Value");
+	  System.out.printf("%s%8s%n","Index","Value");
 	  
 	  for(int counter=0;counter<array.length;counter++) {
 		  
-		  System.out.printf("%5d%8d%n",counter,array[counter]);
-		  
-		  
-		  
+		 System.out.printf("%5d%8d%n",counter,array[counter]); 
 	  }
-	  
 	  
 	  
 	  
