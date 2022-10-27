dd2f711c2c7be73c76cc2445edc3e4a81851ca2e
aidata <aidata@10.121.3.199>
2022-10-27 10:10:09 +0300

    asdasdasd

--- hafta4/src/hafta4/diziler.java
+++ hafta4/src/hafta4/diziler.java
@@ -2,4 +2,39 @@ package hafta4;
 
 public class diziler {
 
+     
+	
+  public static void main(String[]args) {
+	  
+	  int[] array=new int[10];
+	  
+	  
+	  System.out.printf("%s%8%n","Index","Value");
+	  
+	  for(int counter=0;counter<array.length;counter++) {
+		  
+		  System.out.printf("%5d%8d%n",counter,array[counter]);
+		  
+		  
+		  
+	  }
+	  
+	  
+	  
+	  
+  }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
